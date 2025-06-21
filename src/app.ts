716b4a4c8cdac7f2829c2b240872c71e467f16da
Rashedin-063 <rashedinislam.06@gmail.com>
2025-06-21 20:24:34 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 
-const app: Application = express();
 import cors from 'cors';
+const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 
@@ -10,10 +10,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res:Response) => {
   res.send('Hello, World!');
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+module.exports = app; 
--- src/server.ts
+++ src/server.ts
@@ -2,3 +2,12 @@ import mongoose from 'mongoose';
 
 console.log('mongoose version:', mongoose.version);
 
+async function main() {
+  try {
+   
+    
+  } catch (error) {
+    console.error('Error running server:', error);
+    
+  }
+}
