696be81d4105ebb8dd75eb912c070df4d929fd39
Rashedin-063 <rashedinislam.06@gmail.com>
2025-06-21 20:30:50 +0600

    server separated from app

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express';
 
 import cors from 'cors';
 const app: Application = express();
-const PORT = process.env.PORT || 3000;
+
 
 
 // Middleware
@@ -14,9 +14,5 @@ app.get('/', (req: Request, res:Response) => {
   res.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
 
-
-module.exports = app; 
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose';
+import app from './app';
+import dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
-console.log('mongoose version:', mongoose.version);
 
 async function main() {
   try {
    
+    app.listen(process.env.PORT || 3000, () => { 
+      console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`);
+    })
     
   } catch (error) {
     console.error('Error running server:', error);
