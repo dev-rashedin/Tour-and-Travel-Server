3f7cbfc0372f899ca9e579759c18344ac0a4fd1d
Rashedin-063 <rashedinislam.06@gmail.com>
2025-06-22 21:24:49 +0600

    code organized

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
 import mongoose from 'mongoose';
 import app from './app';
-import dotenv from 'dotenv';
-dotenv.config();
-const PORT = process.env.PORT || 3000;
+import { PORT } from './App/Config';
+
 
 
 async function main() {
   try {
    
     app.listen(process.env.PORT || 3000, () => { 
-      console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`);
+      console.log(`Server is running on http://localhost:${PORT}`);
     })
     
   } catch (error) {
